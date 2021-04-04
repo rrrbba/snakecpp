053e60fa5ca9629b71a056a0bfa4162b3f6ad45f
Rayven <rayven.burns@gmail.com>
2021-04-03 20:45:09 -0400

    added initial values for vars in setup

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 using namespace std;
 
-bool gameOver; //global var
-
+//global var
+bool gameOver; 
+const int width = 20; //map dimensions
+const int height = 20; //map dimensions
+int x, y, fruitX, fruitY, score; // x and y are head position
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
 
 void Setup(){
 
     gameOver = false;
+    dir = STOP; //snake won't start moving until a button is pressed
+    x = width / 2;
+    y = height / 2; //snake head is centered on map
+    fruitX = rand() % width; //random position from 0 - width - 1
+    fruitY = rand() % height;
+    score = 0;
 }
 
 void Draw(){
-
+    
 }
 
 void Input(){
@@ -27,7 +38,9 @@ int main(){
     while(!gameOver){
         Draw();
         Input();
-        Logic();    
+        Logic();
+
+        //Sleep(10)    
     }
 
 
