7a3e4aa24105043ef283457d89ac1334330a8c18
Rayven <rayven.burns@gmail.com>
2021-04-03 20:34:51 -0400

    initial setup

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool gameOver; //global var
+
+
+void Setup(){
+
+    gameOver = false;
+}
+
+void Draw(){
+
+}
+
+void Input(){
+
+}
+
+void Logic(){
+
+}
+
+int main(){
+
+    cout << "hello" << endl;
+
+
+    return 0;
+}
