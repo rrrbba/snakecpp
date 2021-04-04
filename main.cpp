544d18a765f85f4c83ef26f00c19897dd6255e01
Rayven <rayven.burns@gmail.com>
2021-04-03 21:04:04 -0400

    working on it

--- main.cpp
+++ main.cpp
@@ -21,7 +21,34 @@ void Setup(){
 }
 
 void Draw(){
-    
+    //Clears screen -> in linux system("clear")
+    system("cls"); 
+    //Display border of map 
+    for (int i = 0; i < width+2; i++){
+        cout << "#"; 
+    }
+    cout << endl;
+
+    for (int i=0; i<height; i++){
+        for (int j=0; j<width; j++){
+            if (j == 0){
+                cout << "#";
+            } else {
+                cout << " ";
+            }
+
+            if (j == width-1) {
+                cout << "#";
+            }
+        }
+        cout << endl;
+    }
+
+
+    for (int i = 0; i < width; i++){
+        cout << "#";
+    }
+    cout << endl;
 }
 
 void Input(){
